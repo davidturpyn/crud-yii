b67485197423857b56950612bf6d457acf3b32be
davidturpyn <davidapricioturpyn@gmail.com>
2022-07-05 00:47:25 +0700

    berhasil migration pertama

---
+++ console/migrations/m220704_160004_create_posts_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%posts}}`.
+ */
+class m220704_160004_create_posts_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%posts}}', [
+            'id' => $this->primaryKey(),
+        ]);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropTable('{{%posts}}');
+    }
+}
